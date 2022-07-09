a9581972210919a3b57c772cff519d8b92c6832b
vincentvega614 <vincentvega614@gmail.com>
2022-07-09 21:47:27 +0300

    исправил файл settings.py_v3

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -115,7 +115,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 # STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static/'),)
-STATICFILES_DIRS = [BASE_DIR + '/api/static/']
+# STATICFILES_DIRS = [BASE_DIR + '/api/static/']
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
