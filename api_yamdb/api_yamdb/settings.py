3885c3d25e54a766978773256460f5805399b0a4
vincentvega614 <vincentvega614@gmail.com>
2022-07-10 13:04:24 +0300

    .gitignore

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -116,7 +116,7 @@ STATIC_URL = '/static/'
 
 # STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static/'),)
 # STATICFILES_DIRS = [BASE_DIR + '/api/static/']
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
